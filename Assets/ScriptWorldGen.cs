27ac55fb80ddfee24714888bfed63f322c892e66
Ravenholdt <joppe4899@hotmail.com>
2016-07-31 21:37:42 +0200

    First!

---
+++ Assets/ScriptWorldGen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScriptWorldGen : MonoBehaviour {
+
+	public GameObject starPrefab;
+	public List<GameObject> starList; //List of all existing stars
+	Quaternion quat;
+
+	// Use this for initialization
+	void Start () {
+		starList = new List<GameObject>();
+		float x;
+		float y;
+
+		for (int i = 0; i < 5; i++) {
+			x = Random.value * 10 - 5;
+			y = Random.value * 10 - 5;
+			Instantiate (starPrefab, new Vector2 (x, y), quat);
+		}
+
+		//Vector2.Distance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
